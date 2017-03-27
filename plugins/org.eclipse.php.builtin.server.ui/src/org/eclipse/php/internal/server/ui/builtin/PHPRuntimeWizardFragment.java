0052c9999e5de2c475ae8c7dd192ebfd4731effb
ibazzi <ibazzi@qq.com>
2017-03-27 08:46:29 +0800

    refactor

---
+++ plugins/org.eclipse.php.builtin.server.ui/src/org/eclipse/php/internal/server/ui/builtin/PHPRuntimeWizardFragment.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Zend Technologies
+ *******************************************************************************/
+package org.eclipse.php.internal.server.ui.builtin;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wst.server.core.IRuntimeWorkingCopy;
+import org.eclipse.wst.server.core.TaskModel;
+import org.eclipse.wst.server.ui.wizard.IWizardHandle;
+import org.eclipse.wst.server.ui.wizard.WizardFragment;
+
+/**
+ * 
+ */
+public class PHPRuntimeWizardFragment extends WizardFragment {
+	protected PHPRuntimeComposite comp;
+
+	public PHPRuntimeWizardFragment() {
+		// do nothing
+	}
+
+	public boolean hasComposite() {
+		return true;
+	}
+
+	public boolean isComplete() {
+		IRuntimeWorkingCopy runtime = (IRuntimeWorkingCopy) getTaskModel().getObject(TaskModel.TASK_RUNTIME);
+
+		if (runtime == null)
+			return false;
+		IStatus status = runtime.validate(null);
+		return (status == null || status.getSeverity() != IStatus.ERROR);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.wst.server.ui.task.WizardFragment#createComposite()
+	 */
+	public Composite createComposite(Composite parent, IWizardHandle wizard) {
+		comp = new PHPRuntimeComposite(parent, wizard);
+		return comp;
+	}
+
+	public void enter() {
+		if (comp != null) {
+			IRuntimeWorkingCopy runtime = (IRuntimeWorkingCopy) getTaskModel().getObject(TaskModel.TASK_RUNTIME);
+			comp.setRuntime(runtime);
+		}
+	}
+
+	public void exit() {
+		// IRuntimeWorkingCopy runtime = (IRuntimeWorkingCopy)
+		// getTaskModel().getObject(TaskModel.TASK_RUNTIME);
+		// IPath path = runtime.getLocation();
+		// if (runtime.validate(null).getSeverity() != IStatus.ERROR)
+		// TomcatPlugin.setPreference("location" +
+		// runtime.getRuntimeType().getId(), path.toString());
+	}
+}
